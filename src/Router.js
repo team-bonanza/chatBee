0770b42916a2facc9d2d3e3aaa217012e26f16e0
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-02-02 00:50:01 +0300

    React-navigation&ui-Kitten

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-export default function Router() {
-  return (
-    <View>
-      <Text>Merhaba Sexy MertKaan</Text>
-    </View>
-  );
-}
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.light}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
