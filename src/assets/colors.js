72fb0e7009f7241a507daebe3bd4e94c586bd9a1
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-02-07 22:17:12 +0300

    image background with dark and light mode

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -6,3 +6,12 @@ export default {
   BACGROUND_LIGHT: ['#FFF', '#abbaab'],
   BACKGROUND_DARK: ['#243b55', '#141e30'],
 };
+
+// const BACKGROUND_DARK = [
+//     '#243b55',
+//     '#20344c',
+//     '#1c2c42',
+//     '#182539',
+//     '#141e30',
+//   ];
+//   const BACKGROUND_LIGHT = ['#FFF', '#edf0ed', '#d8ded8', '#c5cfc5', '#abbaab'];
--- src/components/BeeView.js
+++ src/components/BeeView.js
@@ -4,17 +4,8 @@ import LinearGradient from 'react-native-linear-gradient';
 import {beeView_style} from '../assets/styles';
 
 function BeeView({children}) {
-  const BACKGROUND_DARK = [
-    '#243b55',
-    '#20344c',
-    '#1c2c42',
-    '#182539',
-    '#141e30',
-  ];
-  const BACKGROUND_LIGHT = ['#FFF', '#edf0ed', '#d8ded8', '#c5cfc5', '#abbaab'];
-
-  const imageDark = {uri: '../assets/bgDark.jpg'};
-  const imageLight = {uri: '../assets/bgLightTwo.jpg'};
+  const imageDark = require('../assets/bgDark.jpg');
+  const imageLight = require('../assets/bgLightTwo.jpg');
 
   const theme = Appearance.getColorScheme();
   const image = theme === 'dark' ? imageDark : imageLight;
