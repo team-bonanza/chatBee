d1a887a549253b896c3df6459f6c94b675fcaed3
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-02-07 21:33:30 +0300

    new_gradient

--- src/components/BeeView.js
+++ src/components/BeeView.js
@@ -4,8 +4,8 @@ import LinearGradient from 'react-native-linear-gradient';
 import {beeView_style} from '../assets/styles';
 
 function BeeView({children}) {
-  const BACKGROUND_DARK = ['#243b55', '#141e30'];
-  const BACKGROUND_LIGHT = ['#FFF', '#abbaab'];
+  const BACKGROUND_DARK = ['#243b55','#20344c','#1c2c42','#182539', '#141e30'];
+  const BACKGROUND_LIGHT = ['#FFF','#edf0ed','#d8ded8','#c5cfc5', '#abbaab'];
 
   const theme = Appearance.getColorScheme();
   const BACKGROUND = theme === 'dark' ? BACKGROUND_DARK : BACKGROUND_LIGHT;
