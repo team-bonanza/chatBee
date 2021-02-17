43557cdf228e0aa6b03e53340f409344343d0afc
mertkaanerdem <mmertkaanerdem@gmail.com>
2021-02-18 01:01:28 +0300

    lobi kısmı

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import BeeView from '../components/BeeView';
+
+//TODO: En üstteki View BeeView olacak. unutma
 
 function Lobby() {
   return (
-    <View>
-      <Text>Lobi Sayfası</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-evenly',
+      }}>
+      <View>
+        <Text>Üst Kısım | Logo ile Kodun Kopyalanacağı Kısım Olacak</Text>
+      </View>
+      <View>
+        <Text>Orta Kısım | Kullanıcılar Bu Kısımda Gözükecekler</Text>
+      </View>
+      <View>
+        <Text>
+          Alt Kısım | Kullanıcılar Hazır Olup Olmadıklarını Buradaki Tuşlarla
+          Kontrol Edebilecekler
+        </Text>
+      </View>
     </View>
   );
 }
