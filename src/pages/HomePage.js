44b47606f3579e65370fc4dfb87e4ad823e2f0e6
berkaykrc <berkay.kirci@gmail.com>
2021-03-13 01:19:17 +0300

    joinroom user add to firestore

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -47,12 +47,23 @@ function HomePage({navigation}) {
   async function onNavigateToJoin(screen) {
     const roomRef = db.collection('rooms').doc(roomId);
     const roomSnapshot = await roomRef.get();
-    console.log('roomref', roomRef);
-    console.log('roomsp', roomSnapshot);
 
     if (!roomSnapshot.exists) {
       console.log('oda yok');
     }
+
+    db.collection('lobby')
+      .doc(roomId)
+      .collection('users')
+      .doc(`${auth().currentUser.uid}`)
+      .set(
+        {
+          displayName: auth().currentUser.displayName,
+          isReady: false,
+          photoURL: auth().currentUser.photoURL,
+        },
+        {merge: true},
+      );
 
     navigation.navigate(screen, {
       id: roomId,
